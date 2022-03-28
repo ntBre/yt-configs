b6da2d1276add0b69515d7f6677835db38d99f8c
Brent Westbrook <brent@bwestbro.com>
2022-03-27 19:46:51 -0500

    add emacs configs

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rustic consult flycheck go-mode company which-key lsp-mode treemacs-evil winum treemacs marginalia orderless vertico use-package rainbow-mode evil))
+ '(safe-local-variable-values '((eval local-set-key (kbd "C-c i") #'consult-outline))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ .emacs.d/init.el
@@ -0,0 +1,143 @@
+;; -*- lexical-binding: t; outline-regexp: ";;;"; eval: (local-set-key (kbd "C-c i") #'consult-outline); -*-
+
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+;;; evil
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode)
+  (evil-set-undo-system 'undo-redo)
+  (defmacro brw/fix-evil-hook (hook state key fn)
+    `(add-hook ',hook
+	       (lambda ()
+		 (evil-local-set-key ',state (kbd ,key) ',fn))))
+  (brw/fix-evil-hook xref--xref-buffer-mode-hook motion "RET" xref-goto-xref)
+  (brw/fix-evil-hook flycheck-error-list-mode-hook motion "RET" flycheck-error-list-goto-error)
+  (mapcar
+   #'(lambda (lst)
+       (evil-set-initial-state (car lst) (cdr lst)))
+   '((xref--xref-buffer-mode   . motion)
+     (flycheck-error-list-mode . motion))))
+
+(use-package rainbow-mode
+  :ensure t)
+
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode))
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+(use-package orderless
+  :ensure t
+  :config
+  (setq completion-styles '(orderless)))
+
+(use-package consult
+  :ensure t)
+
+(use-package treemacs
+  :ensure t)
+
+(use-package treemacs-evil
+  :ensure t)
+
+(use-package winum
+  :ensure t
+  :config
+  (global-set-key (kbd "M-0") 'treemacs-select-window)
+  (global-set-key (kbd "M-1") 'winum-select-window-1)
+  (global-set-key (kbd "M-2") 'winum-select-window-2)
+  (global-set-key (kbd "M-3") 'winum-select-window-3)
+  (global-set-key (kbd "M-4") 'winum-select-window-4)
+  (global-set-key (kbd "M-5") 'winum-select-window-5)
+  (global-set-key (kbd "M-6") 'winum-select-window-6)
+  (global-set-key (kbd "M-7") 'winum-select-window-7)
+  (global-set-key (kbd "M-8") 'winum-select-window-8)
+  (winum-mode))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(use-package lsp-mode
+  :ensure t
+  :bind (:map lsp-mode-map
+	      ("C-c d" . lsp-describe-thing-at-point)
+	      ("C-c a" . lsp-execute-code-action))
+  :bind-keymap ("C-c l" . lsp-command-map)
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package company
+  :ensure t
+  :hook ((emacs-lisp-mode . (lambda ()
+			      (setq-local company-backends '(company-elisp))))
+	 (emacs-lisp-mode . company-mode))
+  :config
+  (company-keymap--unbind-quick-access company-active-map)
+  (company-tng-configure-default)
+  (setq company-idle-delay 0.1
+	company-minimum-prefix-length 1))
+
+(use-package flycheck
+  :ensure t)
+
+;;; Rust
+(use-package rustic
+  :ensure t
+  :bind (("<f6>" . rustic-format-buffer))
+  :config
+  (require 'lsp-rust)
+  (setq lsp-rust-analyzer-completion-add-call-parenthesis nil))
+
+;;; Go
+(use-package go-mode
+  :ensure t
+  :hook ((go-mode . lsp-deferred)
+	 (go-mode . company-mode))
+  :bind (:map go-mode-map
+	      ("<f6>"  . gofmt)
+	      ("C-c 6" . gofmt))
+  :config
+  (require 'lsp-go)
+  ;; https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md
+  (setq lsp-go-analyses
+	'((fieldalignment . t)
+	  (nilness        . t)
+	  (unusedwrite    . t)
+	  (unusedparams   . t)))
+  ;; GOPATH/bin
+  (add-to-list 'exec-path "~/go/bin")
+  (setq gofmt-command "goimports"))
+
+(global-set-key (kbd "<f5>") #'recompile)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file t)
+
+(setq auto-save-file-name-transforms
+      '((".*" "~/.emacs.d/auto-save-list/" t))
+      backup-directory-alist
+      '(("." . "~/.emacs.d/backups")))
+
+(set-face-attribute 'region nil :background "deep sky blue")
+(set-face-attribute 'default nil :height 140)
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(put 'narrow-to-region 'disabled nil)
